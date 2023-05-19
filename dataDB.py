bb1dff4427973c8d482d30af5ce2fe9dc7e2699f
VincentLi1216 <sunnus.tw@gmail.com>
2023-05-19 14:28:14 +0800

    add the get and update local data func

--- dataDB.py
+++ dataDB.py
@@ -1,11 +1,6 @@
-import json, pymysql
+import json
 
-player_data = {"name": "Vincent", "account":"sunnus.tw@gmail.com", "pw":"aaabbc123", "coin":300, "characters":["bee", "bird", "cat", "dog"], "level":2}
-
-with open("local_data.json", "w", encoding='utf-8') as f:
-  # json.dump(dict_, f) # 写为一行
-  json.dump(player_data, f, indent=2, sort_keys=True, ensure_ascii=False) # 写为多行
-
+# player_data = {"account": "Vincent", "pw":"aaabbc123", "coin":300, "characters":["bee", "bird", "cat", "dog"], "level":2}
 # 資料庫設定
 db_settings = {
     "host": "server.gems.com.tw",
@@ -15,12 +10,43 @@ db_settings = {
     "db": "Egg_Defense",
     "charset": "utf8"
 }
-try:
-    # 建立Connection物件
-    conn = pymysql.connect(**db_settings)
-    # 建立Cursor物件
-    with conn.cursor() as cursor:
-        pass
-      #資料表相關操作
-except Exception as ex:
-    print(ex)
+
+def connection_test():
+    try:
+        import pymysql
+
+        try:
+            # 建立Connection物件
+            conn = pymysql.connect(**db_settings)
+            # 建立Cursor物件
+            with conn.cursor() as cursor:
+                return True
+            # 資料表相關操作
+        except Exception as ex:
+            return False
+
+    except:
+        return False
+
+
+def update_data(data):
+    if(connection_test()):
+        pass #update the sql data
+    else:
+        with open("local_data.json", "w", encoding='utf-8') as f:
+          json.dump(data, f, indent=2, sort_keys=True, ensure_ascii=False)
+
+
+def get_data():
+    if(connection_test()):
+        pass #get from the sql data
+    else:
+        with open('local_data.json') as f:
+            return json.load(f)
+
+
+
+if __name__ == "__main__":
+    print(connection_test())
+    print(get_data())
+
