0ab69338131022ccef2801f530ef2d63f2c44c15
VincentLi1216 <sunnus.tw@gmail.com>
2023-05-26 23:46:37 +0800

    set enemies all infos to dict

--- test1.py
+++
@@ -1,7 +0,0 @@
-enemiesInfo = {"Crabby":{"HP":200},
-             "Fierce Tooth":200,
-             "Pink Star":30,
-             "Seashell": 200,
-             "Whale":200}
-
-print(enemiesInfo["Crabby"]["HP"])
