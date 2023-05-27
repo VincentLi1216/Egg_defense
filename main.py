dfa4b9472f6fe9ecb22ba494c4676aede403ddf2
reeve-zc <reeve0319@gmail.com>
2023-05-27 12:59:44 +0800

    move card cd time to heroInfo dictionary

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ coordinate = [[0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0]]
 
-heroesInfo = {"dog": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 7, "speed_y": 0},
-              "cat": {"hp": 500, "damage": 100, "fps": 200},
-              "mushroom": {"hp": 1000, "damage": 100, "fps": 200},
-              "bee": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 5, "speed_y": (-5, 0, 5)},
-              "rino": {"hp": float('inf'), "damage": 100, "fps": 10},
-              "bird": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 7, "speed_y": 0},
-              "frog": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 8, "speed_y": 0},
-              "fox": {"hp": 500, "damage": 100, "fps": 100},
-              "turtle": {"hp": 100, "damage": 100, "fps": 100},
-              "turkey": {"hp": float('inf'), "damage": 100, "fps": 10}
+heroesInfo = {"dog": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 7, "speed_y": 0, "cardCD": 500},
+              "cat": {"hp": 500, "damage": 100, "fps": 200, "cardCD": 100},
+              "mushroom": {"hp": 1000, "damage": 100, "fps": 200, "cardCD": 1000},
+              "bee": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 5, "speed_y": (-5, 0, 5), "cardCD": 100},
+              "rino": {"hp": float('inf'), "damage": 100, "fps": 10, "cardCD": 1000},
+              "bird": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 7, "speed_y": 0, "cardCD": 500},
+              "frog": {"hp": 100, "damage": 100, "fps": 100, "speed_x": 8, "speed_y": 0, "cardCD": 500},
+              "fox": {"hp": 500, "damage": 100, "fps": 100, "cardCD": 100},
+              "turtle": {"hp": 100, "damage": 100, "fps": 100, "cardCD": 100},
+              "turkey": {"hp": float('inf'), "damage": 100, "fps": 10, "cardCD": 300}
               }
 
 enemiesInfo = {"Crabby":{"hp":200, "damage":5, "fps":90, "speed":-5, "attack_fps":1000, "attack_moving_speed":-5},
@@ -22,12 +22,6 @@ enemiesInfo = {"Crabby":{"hp":200, "damage":5, "fps":90, "speed":-5, "attack_fps
              "Pink Star":{"hp":30, "damage":5, "fps":90, "speed":-5, "attack_fps":1000, "attack_moving_speed":-30},
              "Seashell": {"hp":200, "damage":30, "fps":90, "speed":-1, "attack_fps":1000, "attack_moving_speed":-5},
              "Whale":{"hp":200, "damage":5, "fps":90, "speed":-2, "attack_fps":2000, "attack_moving_speed":-7}}
-
-cardCD = {"dog": 500, "cat": 100, "mushroom": 1000, "bee": 100, "rino": 1000,
-          "bird": 500, "frog": 500, "fox": 100, "turtle": 100, "turkey": 500}
-
-cardCost = {"dog": 500, "cat": 100, "mushroom": 1000, "bee": 1000, "rino": 3000,
-            "bird": 500, "frog": 500, "fox": 700, "turtle": 1000, "turkey": 900}
 
 def pos2coord(pos):
     x = int((pos[1] - 97.6584) / 124.7363)
@@ -524,14 +518,12 @@ class Whale(Enemy):
 
 class Card:
     def __init__(self, animal):
-        # super().__init__()
         self.animal = animal
         self.surface = [pygame.image.load(f"image/card/card_{self.animal}{i}.png").convert_alpha() for i in range(10)]
         self.rect = self.surface[0].get_rect(topleft=(0, 12.4222))
         self.image = self.surface[0]
         self.index = 0
-        self.fps = cardCD[self.animal]
-        self.cost = cardCost[self.animal]
+        self.fps = heroesInfo[self.animal]["cardCD"]
 
     def cdTime(self):
         if self.index < 9:
