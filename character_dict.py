b99b3284cba0c654703162d7f145dd99cf30fb5a
VincentLi1216 <sunnus.tw@gmail.com>
2023-05-30 10:37:05 +0800

    finish level 1 game design

--- character_dict.py
+++ character_dict.py
@@ -16,8 +16,8 @@ heroesInfo = {"dog": {"hp": 500, "damage": 100, "fps": 100, "speed_x": 7, "speed
               "turkey": {"hp": float('inf'), "damage": 100, "fps": 10, "cardCD": 1200}
               }
 
-enemiesInfo = {"Crabby":{"hp":600, "damage":20, "fps":90, "speed":-5, "attack_fps":1000, "attack_moving_speed":-5},
-             "Fierce Tooth":{"hp":800, "damage":15, "fps":90, "speed":-7, "attack_fps":1000, "attack_moving_speed":-5},
-             "Pink Star":{"hp":500, "damage":30, "fps":90, "speed":-5, "attack_fps":1000, "attack_moving_speed":-30},
-             "Seashell": {"hp":1000, "damage":100, "fps":90, "speed":-1, "attack_fps":1000, "attack_moving_speed":-5},
-             "Whale":{"hp":1000, "damage":10, "fps":90, "speed":-2, "attack_fps":2000, "attack_moving_speed":-7}}
+enemiesInfo = {"Crabby": {"hp": 600, "damage": 20, "fps": 90, "speed": -5, "attack_fps": 1000, "attack_moving_speed": -5},
+               "Fierce Tooth": {"hp": 800, "damage": 15, "fps": 90, "speed": -7, "attack_fps": 1000, "attack_moving_speed": -5},
+               "Pink Star": {"hp": 500, "damage": 30, "fps": 90, "speed": -5, "attack_fps": 1000, "attack_moving_speed": -30},
+               "Seashell": {"hp": 1000, "damage": 100, "fps": 90, "speed": -1, "attack_fps": 2000, "attack_moving_speed": -5},
+               "Whale": {"hp": 1000, "damage": 10, "fps": 90, "speed": -2, "attack_fps": 2000, "attack_moving_speed": -7}}
--- level_design.py
+++ level_design.py
@@ -1,9 +1,13 @@
 level_design = {1: [{"enemy": "Crabby", "time": 0, "row": 0},
                     {"enemy": "Fierce Tooth", "time": 0, "row": 1},
-                    {"enemy": "Seashell", "time": 1, "row": 4},
-                    {"enemy": "Whale", "time": 2, "row": 2},
-                    {"enemy": "Pink Star", "time": 3, "row": 3},
-                    {"enemy": "Whale", "time": 4, "row": 2},
+                    {"enemy": "Seashell", "time": 7, "row": 3},
+                    {"enemy": "Fierce Tooth", "time": 9, "row": 2},
+                    {"enemy": "Crabby", "time": 10, "row": 0},
+                    {"enemy": "Crabby", "time": 10, "row": 4},
+                    {"enemy": "Seashell", "time": 12, "row": 1},
+                    {"enemy": "Fierce Tooth", "time": 12.5, "row": 3},
+                    {"enemy": "Seashell", "time": 13, "row": 2},
+                    {"enemy": "Whale", "time": 13, "row": 4}
                     ],
                 2: [],
                 3: []}
