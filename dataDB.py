db194a055e566c7cdbaf957ba23cffb02258adf6
VincentLi1216 <sunnus.tw@gmail.com>
2023-06-03 15:57:56 +0800

    add more exception handlings

--- dataDB.py
+++ dataDB.py
@@ -31,7 +31,7 @@ def connection_test():
         return False
 
 
-def update_data(data):
+def insert_data(data):
     if (connection_test()):
         # update the sql data
         import pymysql
@@ -44,40 +44,62 @@ def update_data(data):
             cursor.execute(sql, lst)
 
             player_db.commit()
+            data["characters"] = data["characters"].split(",")
+
+            with open("local_data.json", "w", encoding='utf-8') as f:  # update the json file
+                json.dump(data, f, indent=2, sort_keys=True,
+                          ensure_ascii=False)
+
+            return True  # successed
         except:
-            print("i am here")
-            sql = f'UPDATE {table_name} SET timestamp = NOW(), pw = \'{data["pw"]}\', coin = {data["coin"]}, characters = \'{data["characters"]}\', level = {data["level"]} WHERE account = \'{data["account"]}\''
-            print(sql)
-            cursor.execute(sql)
-
-            player_db.commit()
-
-    data["characters"] = data["characters"].split(",")
-
-    with open("local_data.json", "w", encoding='utf-8') as f:
-        json.dump(data, f, indent=2, sort_keys=True, ensure_ascii=False)
+            return False  # failed
 
 
-def get_data(name):
+def update_data(data):
     if (connection_test()):
-        # get from the sql data
+        # update the sql data
         import pymysql
         player_db = pymysql.connect(**db_settings)
         cursor = player_db.cursor()
-        cursor.execute(f"SELECT * FROM {table_name} WHERE account='{name}'")
+        try:
+            sql = f'UPDATE {table_name} SET timestamp = NOW(), pw = \'{data["pw"]}\', coin = {data["coin"]}, characters = \'{data["characters"]}\', level = {data["level"]} WHERE account = \'{data["account"]}\''
+            cursor.execute(sql)
+            player_db.commit()
+            data["characters"] = data["characters"].split(",")
 
-        column_names = [i[0] for i in cursor.description]
-        table_content = cursor.fetchall()
+            with open("local_data.json", "w", encoding='utf-8') as f:
+                json.dump(data, f, indent=2, sort_keys=True,
+                          ensure_ascii=False)
 
-        date = max(table_content, key=lambda x: x[1])
-        player_dict = dict(zip(column_names, list(date)))
-        player_dict["characters"] = player_dict["characters"].split(",")
+            return True
+        except:
+            return False
 
-        return player_dict
 
-    else:
-        with open('local_data.json') as f:
-            return json.load(f)
+def get_data(name):
+    try:
+        if (connection_test()):
+            # get from the sql data
+            import pymysql
+            player_db = pymysql.connect(**db_settings)
+            cursor = player_db.cursor()
+            cursor.execute(
+                f"SELECT * FROM {table_name} WHERE account='{name}'")
+
+            column_names = [i[0] for i in cursor.description]
+            table_content = cursor.fetchall()
+
+            date = max(table_content, key=lambda x: x[1])
+            player_dict = dict(zip(column_names, list(date)))
+            player_dict["characters"] = player_dict["characters"].split(",")
+
+            return player_dict
+
+        else:
+            with open('local_data.json') as f:
+                return json.load(f)
+    except:
+        return False
 
 
 #
@@ -86,5 +108,5 @@ data = {"account": "test_new1", "timestamp": datetime.now().strftime("%Y-%m-%d %
 #
 if __name__ == "__main__":
     print(connection_test())
-    update_data(data)
-    print(get_data("hi"))
+    print(update_data(data))
+    # print(get_data("hi"))
